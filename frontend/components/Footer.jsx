654ec9b25778e289738828b62bfee2d3b49c4bd0
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2022-10-03 22:19:45 +0300

    Fix footer css styles naming

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,10 +1,9 @@
 import styles from '../styles/Footer.module.css';
-import Image from 'next/image';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.footerBackground}>
+      <div className={styles.container}>
         <div className={styles.copyright}>© 2020 MODSHOP Clothing</div>
       </div>
     </div>
