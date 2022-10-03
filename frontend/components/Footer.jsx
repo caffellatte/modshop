cd2ced87862da02201c4f8dec295c95428c2b4a5
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2022-10-03 22:16:24 +0300

    Fixed footer background-image

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,9 +1,12 @@
 import styles from '../styles/Footer.module.css';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div className={styles.copyright}>© 2020 MODSHOP Clothing</div>
+      <div className={styles.footerBackground}>
+        <div className={styles.copyright}>© 2020 MODSHOP Clothing</div>
+      </div>
     </div>
   );
 }
